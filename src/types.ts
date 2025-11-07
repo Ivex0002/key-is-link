75d3fa276fe011b9461780c896add4f237dbecc5
ivex0002 <garam199803@gmail.com>
2025-11-07 19:12:54 +0900

    fix:nullable req optional 제거

--- src/types.ts
+++ src/types.ts
@@ -9,23 +9,22 @@ export type HttpMethod =
 
 // HttpMethod 일때 req 유무에 따른 분기처리
 type MethodHandler<T, Config> =
-  // 먼저 no-req (단순 0인자): config조차 없이 단순 호출
-  T extends () => { res: infer S }
+  // : 데이터 없이 config만 – null/config 강제
+  T extends (req: null, config: Config) => { res: infer S }
+    ? (data: null, config: Config) => Promise<S>
+    : T extends (req: null, config: Config) => {}
+    ? (data: null, config: Config) => Promise<void>
+    : // no-req – 단순 호출
+    T extends () => { res: infer S }
     ? () => Promise<S>
-  : T extends () => {}
+    : T extends () => {}
     ? () => Promise<void>
-  // 그 다음 null req 케이스: body 없음, null 명시 강제로 config 보내기
-  : T extends (req?: null) => { res: infer S }
-    ? (data?: null, config?: Config) => Promise<S>
-  : T extends (req?: null) => {}
-    ? (data?: null, config?: Config) => Promise<void>
-  // 마지막 with-req: body 있음
-  : T extends (req: infer Q) => { res: infer S }
+    : // with-req - 데이터, config 호출 가능
+    T extends (req: infer Q) => { res: infer S }
     ? (data?: Q, config?: Config) => Promise<S>
-  : T extends (req: infer Q) => {}
+    : T extends (req: infer Q) => {}
     ? (data?: Q, config?: Config) => Promise<void>
-  : never;
-
+    : never;
 
 // src\api\apiTree.ts 타입 지정용
 // 들어온 타입객체 키값에 따른 분기처리
